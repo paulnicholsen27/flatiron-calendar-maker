a0d404a032f9fa9e4424ce95021d2e809626f9d4
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2018-08-01 11:17:28 -0400

    fixed route errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'calendars#new'
-  get '/redirect', to: 'calendars#redirect', as: 'redirect'
+  root 'calendars#redirect'
+  # get '/redirect', to: 'calendars#redirect', as: 'redirect'
   get '/callback', to: 'calendars#callback', as: 'callback'
-  get '/calendars', to: 'calendars#list', as: 'calendars'
+  get '/calendars', to: 'calendars#new', as: 'calendars'
   post '/calendars/create', to: 'calendars#create'
 end
